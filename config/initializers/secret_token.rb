87128746e643e8dd9084de2cf4425bbfe7ff9c3d
Ulices Barajas <ulices.barajas@crowdint.com>
2012-10-13 21:01:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HandStore::Application.config.secret_token = '94bc3d89c6ea5190c78a634fe27a2971ebb28b31a76d29ce469dc7ee26c21aab1bbeb8064117b7aa1000079eb197a7ba1ab95e8c08b19e1f3ba7d361dda08e06'
